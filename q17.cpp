0fec7821aa0fd3c3eb4d6f8d758cf1cd2f787c8b
Pin-Ju Tien <pinju.tien@gmail.com>
2016-05-07 09:15:53 -0400

    q17.cpp

--- q17.cpp
+++ q17.cpp
@@ -1,20 +1,38 @@
-#include <iostream>
-#include <vector>
-// http://www.geeksforgeeks.org/connect-nodes-at-same-level/
-// http://www.geeksforgeeks.org/find-height-binary-tree-represented-parent-array/
-// http://www.geeksforgeeks.org/construct-a-binary-tree-from-parent-array-representation/
+class Solution {
+public:
+    vector<string> res;
+    map<char,string>dict;
+    // i : the length of any combination. Once its length = the legth of digits,
+    // it is one of combination (ie "ad") and we store it in the res vector.
+    void functionRe(string digits, int i, string tmp) {
+        if (i == digits.length()) {
+            res.push_back(tmp);
+            return;
+        }
+        string numbers = dict[digits[i]];
+        for (int j = 0; j < numbers.length(); j++) {
+            functionRe(digits, i+1, tmp+numbers[j]);
+        }
 
-int depth(int parent[], int i,int n) {
-
-}
-
-int main (int argc, char * const argv[]) {
-		
-	int v[] = {4, -1 , 4, 1, 1};
-	int nums = sizeof(v)/sizeof(v[0]);
-
-	
-	
-	
-    return 0;
-}
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        dict['2']="abc";
+        dict['3']="def";
+        dict['4']="ghi";
+        dict['5']="jkl";
+        dict['6']="mno";
+        dict['7']="pqrs";
+        dict['8']="tuv";
+        dict['9']="wxyz";
+        if (digits.length() == 0) {return res;}
+        
+        for (int i = 0 ; i < digits.length(); i++) {
+            if ((digits[i] == '1') || (digits[i] == '0') || (digits[i] == '*') || (digits[i] == '#')) {return res;
+            }
+        }
+        // start from the beginning for recursion.
+        functionRe(digits, 0, "");
+        return res;
+    }
+};
