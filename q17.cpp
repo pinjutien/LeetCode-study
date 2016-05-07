b02d647950fa0c37a7b653eddf28d8bdac28e30e
Pin-Ju Tien <pinju.tien@gmail.com>
2016-05-07 09:12:55 -0400

    q17.cpp

---
+++ q17.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+// http://www.geeksforgeeks.org/connect-nodes-at-same-level/
+// http://www.geeksforgeeks.org/find-height-binary-tree-represented-parent-array/
+// http://www.geeksforgeeks.org/construct-a-binary-tree-from-parent-array-representation/
+
+int depth(int parent[], int i,int n) {
+
+}
+
+int main (int argc, char * const argv[]) {
+		
+	int v[] = {4, -1 , 4, 1, 1};
+	int nums = sizeof(v)/sizeof(v[0]);
+
+	
+	
+	
+    return 0;
+}
