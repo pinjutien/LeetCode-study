5be49a89b5f7fe29301ae34c9e13934a13406e11
Pin-Ju Tien <pinju.tien@gmail.com>
2016-05-12 07:40:43 -0400

    4Sum

--- q18.py
+++ q18.py
@@ -1,25 +1,5 @@
-# Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + # c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+# reference: http://chaoren.is-programmer.com/posts/45308.html
 class Solution(object):
-    def threeSumTarget(self, nums, target):
-        nums.sort()
-        if (len(nums) <= 3):
-            return sum(nums)
-        for i in xrange(len(nums)-2):
-            L, R = i +1, len(nums) -1
-            while (L < len(nums) -1) and (R > i) and (L < R) :
-                
-                sum3 = nums[L] + nums[R] + nums[i]
-                diff = abs(sum3 - target)
-                if diff == 0:
-                    return [nums[i], nums[L], nums[R]]
-                    
-                if sum3 <= target:
-                    L += 1
-                else:
-                    R -= 1
-                
-        return []  
- 
     def fourSum(self, nums, target):
         """
         :type nums: List[int]
@@ -27,26 +7,24 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        if len(nums) == 0:
-            return []
-        if (len(nums) <= 4):
-            if (sum(nums) == target):
-                return sum(nums)
-            else: return []    
+        if len(nums) < 4: return []  
 
         nums.sort()
-        if (nums[0] > target) and (nums[0] > 0):
-            return []
-        print nums, len(nums)
-        res = []
-        for i in xrange(len(nums) - 3):
-            print i
-            tmp = []
-            tmp = self.threeSumTarget(nums[i:], target - nums[i])
-            print tmp
-            if len(tmp) != 0:
-                tmp.append(nums[i])
-                tmp.sort()
-                res.append(tmp)
+        lens_nums, dictx = len(nums), {}
+        for i in xrange(lens_nums-1):
+            for j in xrange(i+1, lens_nums):
+                if nums[i] + nums[j] in dictx:
+                    dictx[nums[i] + nums[j]].append((i,j))
+                else:
+                    dictx[nums[i] + nums[j]] = [(i, j)]
 
-        return res
+        result = set()
+        for i in xrange(lens_nums-1):
+            for j in xrange(i+1, lens_nums):
+                temp = target - nums[i] - nums[j]
+                if temp in dictx: # if we use "if temp in dictx.keys()" the Time limit exceeded.
+                    for k in dictx[temp]:
+                        if k[0] > j:
+                            result.add(( nums[i], nums[j], nums[k[0]], nums[k[1]] ))
+        final = [ list(i) for i in result ]
+        return final
