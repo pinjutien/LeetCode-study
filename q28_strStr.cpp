1dfcf40c041e15797be014d6b7f5d19726fc6426
Pin-Ju Tien <pinju.tien@gmail.com>
2016-11-15 07:50:21 -0500

    add Rabin-Karp algorithm

--- q28_strStr.cpp
+++ q28_strStr.cpp
@@ -1,3 +1,7 @@
+#include <string>
+#include <iostream>
+#define kbase 26
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -11,3 +15,48 @@ public:
         
     }
 };
+
+// ref: http://courses.csail.mit.edu/6.006/spring11/rec/rec06.pdf
+int RabinKarp(const string &text, const string &pattern) {
+	if (text.size() < pattern.size()) {
+		return -1;
+	}
+	
+	int prime_number = 101;
+	int text_hash =0;
+	int pattern_hash =0;
+	int power_s = 1;
+	// power_s = pow(kbase, L-1)
+	for (int i = 0; i < pattern.size(); i++) {
+		power_s = i > 0 ? power_s * kbase : 1;
+		text_hash = (text_hash * kbase + text[i])%prime_number;
+		pattern_hash = (pattern_hash * kbase + pattern[i])%prime_number;
+	}
+	
+	for (int i = pattern.size(); i < text.size(); i++) {
+		
+		// if text_hash and patter_hash are the same, we can check if there is
+		// a collision happen.
+		if ( (text_hash == pattern_hash) & (text.compare(i-pattern.size(), pattern.size(), pattern))) {
+			return i - pattern.size();
+		}
+		
+		text_hash = kbase*(text_hash - power_s*text[i - pattern.size()]) + text[i];
+		
+	}
+	
+	if (text_hash = pattern_hash & text.compare(text.size() - pattern.size(), pattern.size(), pattern) == 0) {
+		return text.size() - pattern.size();
+	}
+	
+	return -1;
+}
+
+
+int main() {
+	
+	
+	
+}
+
+
