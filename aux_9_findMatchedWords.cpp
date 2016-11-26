0f9a420e346e46e467182d838caf39be85e24c6c
Pin-Ju Tien <pinju.tien@gmail.com>
2016-11-26 13:29:23 -0500

    fix bug; done

--- aux_9_findMatchedWords.cpp
+++ aux_9_findMatchedWords.cpp
@@ -10,36 +10,37 @@ using std::vector;
 using std::map;
 
 
+
 string decode_pattern(string pattern) {
-  map<string, int> mymap;
-  int current_value = 0;
-  string res = "";
- 
-  int i;
-  for (i = 0; i < pattern.size(); i++) {
-    // when it is not the key in the map
-    if ( mymap.find(pattern[i]) == dict.end()) {
-      current_value += 1;
-      mymap[pattern[i]] = current_value;
-    }
-
-    res = res + to_string(it->second);    
-  }
-
-  return res;
-
+	map<char, int> mymap;
+	int current_value = 0;
+	string res = "";
+	
+	int i;
+	for (i = 0; i < pattern.size(); i++) {
+		// when it is not the key in the map
+		if ( mymap.find(pattern[i]) == mymap.end()) {
+			current_value += 1;
+			mymap[pattern[i]] = current_value;
+		}
+		
+		res = res + to_string(mymap[pattern[i]]);    
+	}
+	
+	return res;
+	
 };
 
 vector<string> findMatchedWords(vector<string> dict, string pattern) {
-  string pattern_code = decode_pattern(pattern);
-  string temp_code;
-  vector<string> final_res;
-  for (int k = 0; k < dict.size(); k++) {
-    temp_code = decode_pattern(dict[i]);
-    if (temp_code == pattern_code) {
-      final_res.push_back(temp_code);
-    }
-  }
-
-  return final_res;
+	string pattern_code = decode_pattern(pattern);
+	string temp_code;
+	vector<string> final_res;
+	for (int k = 0; k < dict.size(); k++) {
+		temp_code = decode_pattern(dict[k]);
+		if (temp_code == pattern_code) {
+			final_res.push_back(temp_code);
+		}
+	}
+	
+	return final_res;
 }
