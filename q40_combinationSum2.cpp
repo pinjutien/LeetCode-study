f911efc66f040c5a37b4ecf880a0748aed5c9787
Pin-Ju Tien <pinju.tien@gmail.com>
2017-01-10 21:51:31 -0500

    combination Sum: every element can only be used once

---
+++ q40_combinationSum2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // ref: http://bangbingsyb.blogspot.com/2014/11/leetcode-combination-sum-i-ii.html
+    void find_target_sum(vector<int>& candidates, int start_index, int target, vector<int>& solution,vector<vector<int>>& all_solutions) {
+        
+        if (target == 0) {
+            all_solutions.push_back(solution);
+            return;
+        }
+        
+        for (int i = start_index; i < candidates.size(); i++) {
+            
+            // skip the i-th element if i- and (i-1)- elements are the same
+            if (candidates[i] == candidates[i-1] & i > start_index) {
+                continue;
+            }
+            
+            if (candidates[i] <= target) {
+                solution.push_back(candidates[i]);
+                find_target_sum(candidates, i+1, target - candidates[i], solution, all_solutions);
+                solution.pop_back();
+            }
+            
+        }
+        
+        
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> all_solutions;
+        vector<int> solution;
+        sort(candidates.begin(), candidates.end());
+        find_target_sum(candidates, 0, target, solution, all_solutions);
+        return all_solutions;
+    }
+};
